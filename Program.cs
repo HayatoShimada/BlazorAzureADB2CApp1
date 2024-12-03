6b1e113ad732b15ba51368dfbe47d51b7c349c08
amdet <amdet@HAYATOSHIMADA>
2024-12-04 00:19:21 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,55 @@
+using BlazorAzureADB2CApp1.Components;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Åö(a) Configure Azure AD B2C Authentication using OpenID Connect
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"))
+    .EnableTokenAcquisitionToCallDownstreamApi()
+    .AddInMemoryTokenCaches();
+
+//Åö(b) Add authorization policy requiring authentication for all controllers
+builder.Services.AddControllersWithViews(options =>
+{
+    var policy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+    options.Filters.Add(new AuthorizeFilter(policy));
+});
+
+//Åö(c) Add Razor Pages and Microsoft Identity UI for authentication-related pages
+builder.Services.AddRazorPages()
+    .AddMicrosoftIdentityUI();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+//Åö(d) Enable routing for authentication-related UI pages
+app.MapControllers();
+app.MapRazorPages();
+
+app.Run();
